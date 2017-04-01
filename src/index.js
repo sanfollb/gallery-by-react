0e142ad31f78605308ac92fbf8d610bc86a65233
sanfollb <sanfollb@163.com>
2017-04-01 17:35:23 +0800

    finish gallery-by-react project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/GalleryByReactApp';
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('app'));
